b47c22aa9c83ebcd5e195e800735b23b838111f0
Alejandro González <Alejandro Gonzlez>
2016-04-24 11:57:34 +0200

    images and footer

--- src/index.php
+++ src/index.php
@@ -8,42 +8,51 @@
       <!-- Main component for a primary marketing message or call to action -->
       <div class="jumbotron">
 
-          <div class="alerta_todo">
-              <div id="alerta">
-                  Durante los próximos <b>tres</b> días hay una alerta por <b>asma</b> en <b>Burgos</b>.
-              </div>
-              <div id="div_close_icon">
-                  <img id="close_icon" src="img/close_icon.png" alt="Close icon">
-                  
+          <div id="alertas">
+              <h2>Alertas</h2>
+              <div class="alerta_todo" id="AlertaPolem">
+                  <div id="alerta">
+                      Durante los próximos <b>tres</b> días hay una alerta por <b>asma</b> en <b>Burgos</b>.
+                  </div>
+                  <div id="div_close_icon">
+                       <img class="close_icon" src="../images/close_icon.png" alt="Close icon">
+                   </div>
+               </div>
+              <div class="alerta_todo" id="AlertaHongos">
+                  <div id="alerta">
+                      Durante los próximos <b>tres</b> días hay una alerta por <b>asma</b> en <b>Burgos</b>.
+                  </div>
+                  <div id="div_close_icon">
+                      <img class="close_icon" src="../images/close_icon.png" alt="Close icon">
+                  </div>
               </div>
           </div>
 
           <h2>Tendencia de la calidad del aire</h2>
           <div class="mapa">
-
-              <select name="fecha">
+              <select name="fecha" id="fecha">
                   <option value="hoy">Hoy</option>
                   <option value="cinco_dias">5 días</option>
               </select>
 
-              <select name="tipoBusqueda">
+              <select name="tipoBusqueda" id="tipoBusqueda">
                   <option value="enfermedadesRespiratorias">Enfermedades Respiratorias</option>
               </select>
 
               <input id="seleccion_mapa" type="submit" value="Submit">
               <br>
 
-              <iframe id="mapa_iframe" width=100% height="550" scrolling="no" frameborder="no" src="https://www.google.com/fusiontables/embedviz?q=select+col1+from+1J5sOkifcHGF1TboV7ci9IgA91tvcXSiBzA6q0_8V&amp;viz=MAP&amp;h=false&amp;lat=40.4167754&amp;lng=-3.7037901999999576&amp;t=1&amp;z=6&amp;l=col1&amp;y=2&amp;tmplt=2&amp;hml=KML"></iframe>
+              <div class="holds_the_iframe">
+                <iframe id="mapa_iframe" width=100% height="550" scrolling="no" frameborder="no" src="https://www.google.com/fusiontables/embedviz?q=select+col1+from+1J5sOkifcHGF1TboV7ci9IgA91tvcXSiBzA6q0_8V&amp;viz=MAP&amp;h=false&amp;lat=40.4167754&amp;lng=-3.7037901999999576&amp;t=1&amp;z=6&amp;l=col1&amp;y=2&amp;tmplt=2&amp;hml=KML"></iframe>
+
+
+              </div>
+              
           </div>
 
       </div>
 
     </div> <!-- /container -->
-
-
-    <div class="div_footer">
-
-    </div>
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
