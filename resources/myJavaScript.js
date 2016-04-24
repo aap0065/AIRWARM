b6966742fc9f826b905c5530f099a000718478e3
Alejandro González <Alejandro Gonzlez>
2016-04-24 11:09:32 +0200

    Index and css updated

--- resources/myJavaScript.js
+++ resources/myJavaScript.js
@@ -1,17 +1,32 @@
 
 
 $(document).ready(function(){
-    
 
-    $("#close_icon").click(function(){
-        $(".alerta_todo").fadeOut("slow");
+    var $actual_map = "hoy";
+    var $numAlertas = "2"
+
+    $(".close_icon").click(function(){
+        $(this).parent().parent().fadeOut("slow");
+        $numAlertas = $numAlertas - 1;
+        if ($numAlertas==0){
+            $("#alertas").fadeOut("slow");
+        }
     });
     $("#seleccion_mapa").click(function () {
-
-        $("#mapa_iframe").prop('src',"https://www.google.com/fusiontables/embedviz?q=select+col2+from+15AsY10kIrrTjIMMoSY2L-eAs9SFEy-HhudcaKA6L&viz=MAP&h=false&lat=40.4167754&lng=-3.7037901999999576&t=1&z=6&l=col2&y=2&tmplt=2&hml=GEOCODABLE");
+        var $nextValue = document.getElementById("fecha").value;
+        if($nextValue!=$actual_map) {
+            if($actual_map == "hoy") {
+                $("#mapa_iframe").prop('src', "https://www.google.com/fusiontables/embedviz?q=select+col2+from+15AsY10kIrrTjIMMoSY2L-eAs9SFEy-HhudcaKA6L&viz=MAP&h=false&lat=40.4167754&lng=-3.7037901999999576&t=1&z=6&l=col2&y=2&tmplt=2&hml=GEOCODABLE");
+                $actual_map = "cinco_dias";
+            }
+            else {
+                $("#mapa_iframe").prop('src', "https://www.google.com/fusiontables/embedviz?q=select+col2+from+15AsY10kIrrTjIMMoSY2L-eAs9SFEy-HhudcaKA6L&viz=MAP&h=false&lat=40.4167754&lng=-3.7037901999999576&t=1&z=6&l=col2&y=2&tmplt=2&hml=GEOCODABLE");
+                $actual_map = "hoy";
+            }
+        }
 
         /* Previsión
-         https://www.google.com/fusiontables/embedviz?q=select+col2+from+15AsY10kIrrTjIMMoSY2L-eAs9SFEy-HhudcaKA6L&viz=MAP&h=false&lat=40.4167754&lng=-3.7037901999999576&t=1&z=6&l=col2&y=2&tmplt=2&hml=GEOCODABLE
+         https://www.google.com/fusiontables/embedviz?q=select+col2+from+15AsY10kIrrTjIMMoSY2L-eAs9SFEy-HhudcaKA6L&viz=MAP&h=false&lat=38.59158034500299&lng=-0.11378515625006003&t=1&z=5&l=col2&y=2&tmplt=2&hml=GEOCODABLE
          */
         /** Normal
          * https://www.google.com/fusiontables/embedviz?q=select+col2+from+15AsY10kIrrTjIMMoSY2L-eAs9SFEy-HhudcaKA6L&viz=MAP&h=false&lat=40.4167754&lng=-3.7037901999999576&t=1&z=6&l=col2&y=2&tmplt=2&hml=GEOCODABLE
@@ -19,4 +34,5 @@ $(document).ready(function(){
     });
 
 
-});
+
+    });
